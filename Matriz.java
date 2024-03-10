c2ec0d9db3070381148af2702018814c2cd11af8
Elmago2001 <132940985+Elmago2001@users.noreply.github.com>
2024-03-09 22:30:51 -0600

    Create Matriz.java

---
+++ Matriz.java
@@ -0,0 +1,25 @@
+//import java.lang.reflect.Array;
+//import java.util.ArrayList;
+public class Matriz {
+
+    public static void main(String[] args) {
+        String [] lista1 ={"Adolfo", "Ogarrio", "Rosas"};
+        int a= lista1.length;
+        for(int i=0; i==0; i++){
+            if (a>i) {
+                System.out.println(" y ");
+                
+            }else{
+
+            }
+
+        }
+        //ArrayList<String> lista= new ArrayList<>();
+        /*lista.add("Adolfo");
+        lista.add("Ogarrio");
+        lista.add(" y ");
+        lista.add("Rosas");
+        System.out.println(lista);*/
+
+    }
+}
