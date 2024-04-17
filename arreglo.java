1e21d473dbae0f2c8dc7c09ac58070260d7eb411
Elmago2001 <132940985+Elmago2001@users.noreply.github.com>
2024-04-16 20:33:16 -0600

    Create arreglo.java

---
+++ arreglo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class arreglo {
+    public static void main(String[] args) {
+        Scanner ingresaDato= new Scanner(System.in);
+        double [] datos=new double[4];
+        for(i=0; i<datos.length; i++){
+            double i =ingresaDato.nextDouble();
+            System.out.println("ingresa valor ");
+        }
+        
+
+    }
+}
